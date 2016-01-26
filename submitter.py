67e19279ee6736cd3f1b40bae2cf154353bc3be8
Lewis Babbage <Lewis@Lewiss-MacBook-Pro.local>
2016-01-26 14:31:30 +0000

    EQ-16 link to queue and s3

--- submitter.py
+++ submitter.py
@@ -1,23 +1,41 @@
 import boto3
 import pika
+import traceback
 
 
-def submit_data(key, data):
-     try:
-          s3 = boto3.resource('s3')
-          s3.Bucket('eq-submissions').put_object(Key=key, Body=data)
-     except Exception:
-          print "You have a hole in your bucket, your bucket"
+def submit_data(ch, method, properties, body):
+    try:
+        s3 = boto3.resource('s3')
+        s3.Bucket('eq-submissions').put_object(Key=body, Body=body)
+    except Exception:
+        print "You have a hole in your bucket, your bucket"
 
+
+
+def create_data(body):
+    credentials = pika.credentials.PlainCredentials('admin', 'admin')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='david-rabbitmq1.eq.ons.digital', credentials=credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue='eq-submissions')
+    channel.basic_publish(exchange='', routing_key='eq-submissions', body=body)
+    connection.close()
 
 def retrieve_data():
-     try:
-          credentials = pika.credentials.PlainCredentials('admin', 'admin')
-          connection = pika.BlockingConnection(pika.ConnectionParameters
-                                          (host='david-rabbitmq1.eq.ons.digital', credentials=credentials))
-          channel = connection.channel()
+    try:
+        credentials = pika.credentials.PlainCredentials('admin', 'admin')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+                                        host='david-rabbitmq1.eq.ons.digital',
+                                        credentials=credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue='eq-submissions')
+        channel.basic_consume(submit_data, queue='eq-submissions')
+        channel.start_consuming()
 
-          for body in channel.consume('hello'):
-               submit_data('json', body)
-     except Exception:
-          print "connection error"
+
+    except Exception:
+        traceback.print_exc()
+        print "connection error"
+
+if __name__ == '__main__':
+    retrieve_data()
--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,15 @@
 import unittest
-from submitter import submit_data, retrieve_data
+from submitter import submit_data, retrieve_data, create_data
 
 
 class SubmitterTest(unittest.TestCase):
 
-    def test_survey_submit(self):
-        submit_data("test", "test")
 
     def test_receive_data(self):
         retrieve_data()
+
+    def test_create_data(self):
+        create_data("filename")
 
 if __name__ == '__main__':
     unittest.main()
