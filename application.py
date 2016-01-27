99083105e93ec052f8eb03eb713715905f9a67f6
David Jones <tafftoo@gmail.com>
2016-01-27 16:50:35 +0000

    More robust rabbitmq consumer, and supervistor config added

--- application.py
+++ application.py
@@ -1,4 +1,13 @@
-from submitter import retrieve_data
+from submission_consumer import SubmissionConsumer
+from submitters import S3Submitter
+import settings
+
+def message_received(message):
+    print('Received: %s', message)
+    return submitter.submit_data(None, message)
 
 if __name__ == '__main__':
-    retrieve_data()
+    consumer = SubmissionConsumer(settings.EQ_RABBITMQ_URL, settings.EQ_RABBITMQ_QUEUE_NAME)
+    submitter = S3Submitter()
+    consumer.message_received=message_received
+    consumer.run()
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 # Setup the connection string: the 2%F refers to the '/' virtual host
 EQ_RABBITMQ_URL = 'amqp://admin:admin@David-RabbitMQ-LB-1784354550.eu-west-1.elb.amazonaws.com:5672/%2F'
-EQ_RABBITMQ_QUEUE_NAME = 'in_settings'
+EQ_RABBITMQ_QUEUE_NAME = 'eq-submissions'
+EQ_SUBMISSIONS_BUCKET_NAME = 'eq-submissions'
